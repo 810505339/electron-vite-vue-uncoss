d02bb1e6afacfee3d6216eee4d74c9076ff4a515
xueming <810505339@qq.com>
2023-02-28 11:28:45 +0800

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const isDark = useDark()
+const toggle = useToggle(isDark)
+function handleClick() {
+  console.log(1);
+  
+  toggle()
+}
+</script>
+
+<template>
+  <div row  justify-center>
+    <button icon-btn @click="handleClick" mr2>
+      <i dark:i-carbon-moon i-carbon-sun block text-xl></i>
+    </button>
+    <i icon-btn i-carbon-logo-github text-xl></i>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import "./style.css"
+import App from './App.vue'
+import './samples/node-api'
+import 'uno.css'
+import '@unocss/reset/antfu.css'
+createApp(App)
+  .mount('#app')
+  .$nextTick(() => {
+    postMessage({ payload: 'removeLoading' }, '*')
+  })
